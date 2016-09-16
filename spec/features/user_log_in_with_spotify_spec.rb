1c4623cf238a5bc337d06b17f0e7461742b002f4
Garrett <garrett.smestad@gmail.com>
2016-09-15 23:57:24 -0600

    Close #6 User login with Spotify
    - Setup user creation by creating User model & table
    - Add current_user helper method in Application Controller
    - Modify homepage to show username if logged in, otherwise link to log in with Spotify

--- spec/features/user_log_in_with_spotify_spec.rb
+++ spec/features/user_log_in_with_spotify_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.feature 'User logs in with Spotify' do
   context 'new user grants access to Spotify profile' do
     scenario 'they visit the root path' do
-      auth_data = {
-          'provider' => 'spotify',
-          'info'     => {
-            'display_name' => 'Fake User',
-            'id'           => '12345'
-          }
-        }
-
-      OmniAuth.config.mock_auth[:spotify] = auth_data
+      # auth_data = {
+      #     'provider' => 'spotify',
+      #     'info'     => {
+      #       'display_name' => 'Fake User',
+      #       'id'           => '12345'
+      #     }
+      #   }
+      #
+      # OmniAuth.config.mock_auth[:spotify] = auth_data
 
       visit root_path
 
