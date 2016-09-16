e5dde6e0f93e79d224138e4a7645102eadea2661
Garrett <garrett.smestad@gmail.com>
2016-09-15 23:11:28 -0600

    Implement Spotify OAuth
    - Set omniauth-oauth2 gem to version 1.3.1 to fix Invalid Redirect URI error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
   def create
+    if user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
+    end
+    redirect_to root_path
   end
 end
