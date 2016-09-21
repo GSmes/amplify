9ea66544302504220dd0d3e7a58fd5f19f5b712e
Garrett <garrett.smestad@gmail.com>
2016-09-21 16:36:09 -0600

    Implement full playlist creation & add navigation links to views

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,19 +1,29 @@
 class Playlist
-  # def initialize
-  #   # code
-  # end
-
-  def self.create_playlist(user, playlist_name)
-    playlist = service.new_playlist(user, playlist_name)
+  def initialize(user, playlist_params)
+    playlist  = create_playlist(user, playlist_params[:playlist_name])
+    songs     = find_songs(user, playlist_params[:artist], playlist_params[:songs])
+    populate_playlist(user, playlist[:id], songs)
   end
 
-  def self.service
-    SpotifyService.new
+  def create_playlist(user, playlist_name)
+    service.new_playlist(user, playlist_name)
+  end
+
+  def find_songs(user, artist, songs_array)
+    songs_array.map do |song|
+      response = service.find_song(user, artist, song)
+      next if response.blank?
+      response[:uri]
+    end.compact
+  end
+
+  def populate_playlist(user, playlist_id, songs)
+    service.add_to_playlist(user, playlist_id, songs)
   end
 
   private
 
-  # def method_name
-  #   # code
-  # end
+  def service
+    SpotifyService.new
+  end
 end
