41923146a38e4625232cdc980b70480b9d9ac01e
Garrett <garrett.smestad@gmail.com>
2016-09-21 22:04:08 -0600

    Improve styling of search page & results page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user,
-                :setlist_service
+  helper_method :current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def setlist_service
-    @setlist_service ||= SetlistService.new
   end
 end
--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -1,6 +1,6 @@
 class SetlistsController < ApplicationController
   def index
-    @query = params['artistName']
+    @query = params[:artist_name]
     @setlists = Setlist.find_all(@query)
   end
 end
